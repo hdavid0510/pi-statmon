e231086dd508782e4859fc78a550601a063a5d72
David Hong <gdavid0510@gmail.com>
2020-07-09 15:20:15 +0900

    Separate print_thermal and print_network_usage

--- statmon_lcd1602_i2c.py
+++ statmon_lcd1602_i2c.py
@@ -48,17 +48,20 @@ def get_network_usage(netinterface='wlan0'):
 	return (rx, tx)
 
 
-def print_info(netup,netdown):
-	with open('/sys/class/thermal/thermal_zone0/temp', 'r') as ft:
-		temp = round(int(ft.read())/1000);
-	l.lcd_display_string_pos('{:2d}'.format(temp),2,0)
-	l.lcd_write(0x80+0x40+0x2); l.lcd_write_char(ICN_DEGC)
+def print_network_usage(netup,netdown):
 	l.lcd_write(0x80+0x40+0x4); l.lcd_write_char(ICN_UP)
 	l.lcd_display_string_pos('{:4f}'.format((  netup/1024)/REFRESH_DELAY),2, 5)
 	l.lcd_write(0x80+0x40+0x9); l.lcd_write_char(ICN_DOWN)
 	l.lcd_display_string_pos('{:4f}'.format((netdown/1024)/REFRESH_DELAY),2,10)
 	l.lcd_write(0x80+0x40+0xf); l.lcd_write_char(ICN_PERSEC)
 	l.lcd_display_string_pos("K",2,14)
+
+
+def print_thermal():
+	with open('/sys/class/thermal/thermal_zone0/temp', 'r') as ft:
+		temp = round(int(ft.read())/1000);
+	l.lcd_display_string_pos('{:2d}'.format(temp),2,0)
+	l.lcd_write(0x80+0x40+0x2); l.lcd_write_char(ICN_DEGC)
 
 
 def main():
@@ -80,16 +83,20 @@ def main():
 			l.lcd_write(0x80+0x00+0x0);	l.lcd_write_char(ICN_LAN)
 			l.lcd_display_string_pos(ip_lan+"        ",1,1)
 			netusage_now_down, netusage_now_up = get_network_usage('eth0')
-			print_info(netusage_now_up - netusage_old_up, netusage_now_down - netusage_old_down)
+			print_network_usage(netusage_now_up - netusage_old_up, netusage_now_down - netusage_old_down)
 			netusage_old_down, netusage_old_up = netusage_now_down, netusage_now_up
 		elif avail_wifi:
 			l.lcd_write(0x80+0x00+0x0); l.lcd_write_char(ICN_WIFI)
 			l.lcd_display_string_pos(ip_wifi+"        ",1,1)
 			netusage_now_down, netusage_now_up = get_network_usage('wlan0')
-			print_info(netusage_now_up - netusage_old_up, netusage_now_down - netusage_old_down)
+			print_network_usage(netusage_now_up - netusage_old_up, netusage_now_down - netusage_old_down)
 			netusage_old_down, netusage_old_up = netusage_now_down, netusage_now_up
 		else:
-			l.lcd_display_string_pos("No network available",1,1)
+			l.lcd_write(0x80+0x00+0x0); l.lcd_write_char(ICN_WIFI)
+			l.lcd_display_string_pos("N/A",1,2)
+			l.lcd_write(0x80+0x00+0x8); l.lcd_write_char(ICN_LAN)
+			l.lcd_display_string_pos("N/A",1,10)
+		print_thermal()
 		sleep(REFRESH_DELAY)
 
 
